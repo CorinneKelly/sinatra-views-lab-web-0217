e354e99f4ca6b9ace6d21799390cfc6f6abe1173
CorinneKelly <corinnemariekelly@gmail.com>
2017-02-19 21:31:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,20 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		status = 200
+		erb :hello
+	end
+
+	get '/goodbye' do
+		status = 200
+		erb :goodbye
+	end
+
+	get '/date' do
+		Date.today.strftime("%A, %B %d, %Y")
+		status = 200
+		erb :date
+	end
 
 end
